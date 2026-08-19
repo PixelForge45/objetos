6e4f03193b39b4a2a5e4e2b3c8a865391e211b8b
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-08-19 15:07:32 +0000

    pontos

---
+++ objetos.js
@@ -0,0 +1,10 @@
+const listaCPF = [1111111, 2222222, 3333333]
+
+const informaçoesPessoas = ["nome", "Jose", "idade", 30, "cpf", "1111111"];
+
+console.log(informaçoesPessoas[5])
+
+const objetopessoa = {
+  idade: 30,
+  nome: "Jose",
+}
