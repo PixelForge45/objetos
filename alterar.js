56b6fcc551277cd0e0776af131837bccc4d13815
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-08-26 15:16:46 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaa

--- alterar.js
+++ alterar.js
@@ -4,3 +4,17 @@ const pessoa = {
 }
 
 console.log(pessoa.nome)
+
+console.log(pessoa.telefone)
+
+pessoa.telefone = "44 22334455"
+
+console.log(pessoa.telefone)
+
+pessoa.nome = "luma silva"
+
+const novaPessoa = {
+    nome: "Pedro"
+}
+
+pessoa = novaPessoa
