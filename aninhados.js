1b26cb62e20b391186ebb4331ec79c32570121ff
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-08-27 15:20:22 +0000

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ aninhados.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome: "joão",
+    idade: 24,
+    email: "joaofirm@gmail.com",
+    telefone: "11 2233445566, 22 113344556677"
+}
+
+cliente.endereco = [
+    {
+        rua: "R. Joseph Climber",
+        numero: 67,
+        apartamento: true,
+        complemento: "ap 93"
+    }
+]
+
+console.log(cliente)
---
+++ tipo.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "joão",
+    idade: 24,
+    email: "joaofirm@gmail.com",
+    telefone: "11 2233445566, 22 113344556677"
+}
+
+console.log(cliente.telefone)
