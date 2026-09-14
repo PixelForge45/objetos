0d6d0717b83acf431f1ab4b49deb3d7fcc7fb32f
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-09-14 15:23:06 +0000

    repetição

--- repeticao.js
+++ repeticao.js
@@ -4,6 +4,7 @@ const cliente = {
     email: "Joao@firma.com",
     telefone: ["115555550", "114444440"],
 };
+
 cliente.enderecos = {
     rua: "Rua Joseph Climber",
     numero: 1337,
@@ -12,5 +13,9 @@ cliente.enderecos = {
 };
 
 for (let chave in cliente) {
-    console.log(`A chave é ${chave} e o valor é ${cliente[chave]}`);
+    let tipo = typeof cliente[chave];
+    if(tipo !== "object" && tipo !== "function"){
+        console.log(`A chave é ${chave} e o valor é ${cliente[chave]}`);
+    }
+
 }
