fcb206aef45b98d176e450081c954b4dfb80f774
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-09-21 15:21:26 +0000

    21/09/2026

--- leitura.js
+++ leitura.js
@@ -2,3 +2,12 @@ const dados = require('./cliente.json');
 
 console.log(dados);
 console.log( typeof dados);
+
+const clienteEmstring = JSON.stringify(dados);
+console.log(clienteEmstring);
+console.log(typeof clienteEmstring);
+
+const clienteEmObjeto = JSON.parse(clienteEmstring);
+
+console.log(clienteEmObjeto);
+console.log(typeof clienteEmObjeto);
