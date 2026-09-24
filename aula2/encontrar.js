7bb620dfcf61d1341768d122faf16c4792c66271
Cristopher <142592196+PixelForge45@users.noreply.github.com>
2026-09-24 15:23:08 +0000

    24/09/2026

--- aula2/encontrar.js
+++ aula2/encontrar.js
@@ -1,1 +1,11 @@
-const cliente = require("./cliente.json");
+const clientes = require("./cliente.json");
+
+function encontrar(lista, chave, valor) {
+    return lista.find(
+        (item) => item[chave].toLowerCase() === valor.toLowerCase()
+    );
+}
+
+const encontrado = encontrar(clientes, "nome", "olva");
+
+console.log(encontrado);
